bfcd527cb344f482181658681a7140345a6dfaf8
Николай <37457255+kokokochet@users.noreply.github.com>
2020-09-10 17:58:54 +0300

    refactoring

--- src/converter/Main.kt
+++ src/converter/Main.kt
@@ -1,85 +1,94 @@
 package converter
 
+import kotlin.collections.HashMap
+
 class Converter {
-    /* 1 - расстояния
-    *  2 - температура
-    *  3 - вес
-    */
-    private val typesOfUnit = hashMapOf(
-            "meter" to 1, "centimeter" to 1, "mile" to 1,
-            "inch" to 1, "millimeter" to 1, "kilometer" to 1,
-            "yard" to 1, "foot" to 1,
 
-            "degree Fahrenheit" to 2, "degree Celsius" to 2, "Kelvin" to 2,
+    enum class UnitType {
 
-            "kilogram" to 3, "pound" to 3,
-            "gram" to 3, "milligram" to 3,
-            "pound" to 3, "ounce" to 3)
-    //При добавлении дополнительных размерностей обязательно обновлять ! typesOfUnit !
-    class Lengths(val inp: Double, val type: String) {
-        val types = hashMapOf(
+        LENGTH {
+            private val types = hashMapOf(
                 "meter" to 1.0,
                 "centimeter" to 100.0, "mile" to (1.0 / 1609.35),
                 "inch" to 39.37, "millimeter" to 1000.0,
                 "kilometer" to (1.0 / 1000), "yard" to (1.0 / 0.9144),
                 "foot" to (1.0 / 0.3048), "inch" to (1.0 / 0.0254))
 
-        fun convertTo(targetType: String): Double {
-            if (inp < 0) {
-                println("Length shouldn't be negative")
-                return Double.NaN
+            override fun convertTo(fromType: String, targetType: String, value: Double): Double {
+                if (value < 0) {
+                    println("Length shouldn't be negative")
+                    return Double.NaN
+                }
+                return (value / types[fromType]!!) * types[targetType]!!
             }
-            return (inp / types[type]!!) * types[targetType]!!
-        }
-    }
 
-    class Temperature(val inp: Double, val type: String) {
-        val types = hashMapOf("degree Celsius" to 0.0,
+            override fun isThis(type: String) = check(type, types)
+        },
+
+        TEMPERATURE {
+            private val types = hashMapOf("degree Celsius" to 0.0,
                 "degree Fahrenheit" to 1.0, "Kelvin" to 2.0)
 
-        fun convertTo(targetType: String): Double {
-            val celsius = when(types[type]) {
-                0.0 -> inp
-                1.0 -> (inp - 32.0) / 1.8
-                2.0 -> inp - 273.15
-                else -> 0.0
+            override fun convertTo(fromType: String, targetType: String, value: Double): Double {
+                val celsius = when(types[fromType]) {
+                    0.0 -> value
+                    1.0 -> (value - 32.0) / 1.8
+                    2.0 -> value - 273.15
+                    else -> 0.0
+                }
+                if (celsius + 273.15 < 0) {
+                    println("Temperature can't be < 0K")
+                    return Double.NaN
+                }
+                return when(types[targetType]) {
+                    0.0 -> celsius
+                    1.0 -> celsius * 1.8 + 32
+                    2.0 -> celsius + 273.15
+                    else -> Double.NaN
+                }
             }
-            if (celsius + 273.15 < 0) {
-                println("Temperature can't be < 0K")
-                return Double.NaN
-            }
-            return when(types[targetType]) {
-                0.0 -> celsius
-                1.0 -> celsius * 1.8 + 32
-                2.0 -> celsius + 273.15
-                else -> Double.NaN
-            }
-        }
-    }
 
-    class Weight(val inp: Double, val type: String) {
-        val types = hashMapOf("gram" to 1.0,
+            override fun isThis(type: String) = check(type, types)
+        },
+
+        WEIGH {
+            private val types = hashMapOf("gram" to 1.0,
                 "kilogram" to 1000.0, "milligram" to 0.001,
                 "pound" to 453.592, "ounce" to 28.3495)
 
-        fun convertTo(targetType: String): Double {
-            if (inp < 0) {
-                println("Weight shouldn't be negative")
-                return Double.NaN
+            override fun convertTo(fromType: String, targetType: String, value: Double): Double {
+                if (value < 0) {
+                    println("Weight shouldn't be negative")
+                    return Double.NaN
+                }
+                return (value * types[fromType]!!) / types[targetType]!!
             }
-            return (inp * types[type]!!) / types[targetType]!!
-        }
+
+            override fun isThis(type: String) = check(type, types)
+        };
+
+        abstract fun isThis(type: String): Boolean
+        abstract fun convertTo(fromType: String, targetType: String, value: Double): Double
+
+        fun check(type: String, types: HashMap<String, out Any>): Boolean = types.containsKey(type)
+    }
+
+    private fun getType(type: String): UnitType? = when {
+        UnitType.WEIGH.isThis(type) -> UnitType.WEIGH
+        UnitType.LENGTH.isThis(type) -> UnitType.LENGTH
+        UnitType.TEMPERATURE.isThis(type) -> UnitType.TEMPERATURE
+        else -> null
     }
 
     fun convert(inp: Double, fromTypeInput: String, toTypeInput: String): Double {
         var fromType = getNormalType(fromTypeInput)
         var toType = getNormalType(toTypeInput)
-        var weCan = typesOfUnit[fromType] == typesOfUnit[toType]
-        if (typesOfUnit[fromType] == null) {
+        var weCan = getType(fromType) == getType(toType)
+        if (getType(fromType) == null) {
             fromType = "???"
             weCan = false
         }
-        if (typesOfUnit[toType] == null) {
+        if (getType(toType) == null) {
             toType = "???"
             weCan = false
         }
@@ -88,12 +97,7 @@ class Converter {
                     "${getMultiForm(toType)} is impossible")
             return Double.NaN
         }
-        return when(typesOfUnit[fromType]!!) {
-            1 -> Lengths(inp, fromType).convertTo(toType)
-            2 -> Temperature(inp, fromType).convertTo(toType)
-            3 -> Weight(inp, fromType).convertTo(toType)
-            else -> 0.0
-        }
+        return getType(fromType)!!.convertTo(fromType, toType, inp)
     }
 
     fun getNormalType(type: String): String {
@@ -120,9 +124,8 @@ class Converter {
         }
     }
 
-    //only after getNormalType()
     fun getMultiForm(type: String): String {
-        return when(type) {
+        return when(getNormalType(type)) {
             "inch" -> "inches"
             "foot" -> "feet"
             "degree Fahrenheit" -> "degrees Fahrenheit"
@@ -134,13 +137,13 @@ class Converter {
 }
 
 val sc = java.util.Scanner(System.`in`)
-val conveter = Converter()
+val converter = Converter()
 
 fun readUnit(): String {
     var type = sc.next().toLowerCase()
     if (type == "degrees" || type == "degree")
         type += " " + sc.next()
-    return conveter.getNormalType(type)
+    return converter.getNormalType(type)
 }
 
 fun solve(inp: Double) {
@@ -148,16 +151,16 @@ fun solve(inp: Double) {
     sc.next()
     var typeTo = readUnit()
 
-    val result = conveter.convert(inp, typeFrom, typeTo)
+    val result = converter.convert(inp, typeFrom, typeTo)
 
     if (result.isNaN()) return
 
-    if (inp != 1.0) typeFrom = conveter.getMultiForm(typeFrom)
-    if (result != 1.0) typeTo = conveter.getMultiForm(typeTo)
+    if (inp != 1.0) typeFrom = converter.getMultiForm(typeFrom)
+    if (result != 1.0) typeTo = converter.getMultiForm(typeTo)
     println("$inp $typeFrom is $result $typeTo")
 }
 
-fun main(agrs: Array<String>) {
+fun main() {
     print("Enter what you want to convert (or exit): ")
     var s = sc.next()
     while(s != "exit") {
